27f3acdc943ad37b7de8de10c9b6451d48b9db2a
Danilo Alfaro <alexander.danilo.alfaro@gmail.com>
2020-02-17 03:12:39 -0600

    ...

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'rails/test_unit/railtie'
-require "active_storage/engine"
-require "action_text/engine"
-
+require 'active_storage/engine'
+require 'action_text/engine'
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
